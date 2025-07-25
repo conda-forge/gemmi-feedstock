78e0dae37ea149ae3664678112f19956d4e6d792
UENO, M. <eunosium-1128@live.jp>
2025-07-26 01:50:16 +0900

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,11 +2,13 @@
 set -e
 
 cmake ${CMAKE_ARGS} \
+    -G Ninja \
     -D CMAKE_BUILD_TYPE=Release \
     -D CMAKE_INSTALL_PREFIX=$PREFIX \
     -D CMAKE_PREFIX_PATH=$PREFIX \
     -D PYTHON_EXECUTABLE=$PYTHON \
     -D USE_PYTHON=ON \
+    -D CMAKE_POLICY_VERSION_MINIMUM=3.5 \
     .
 
 make -j$CPU_COUNT
